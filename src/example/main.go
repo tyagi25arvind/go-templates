522c90326ac709fc85405e4d81795bfe4ac73a89
tyagi25arvind <76576463+tyagi25arvind@users.noreply.github.com>
2021-01-24 08:41:05 +0530

    refresh 24th Jan first

--- src/example/main.go
+++ src/example/main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"os"
+	"log"
+	"net/http"
+	"text/template"
 )
 
 func main() {
-	templateString := `Leonade Stand Supply`
-	t, err := template.New("title").Parse(templateString)
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = t.Execute(os.Stdout, nil)
-	if err != nil {
-		fmt.Println(err)
-	}
+	// http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	template := populateTemplates()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestedFile := r.URL.Path[1:]
+		t := template.Lookup(requestedFile + ".html")
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":8000", nil)
+}
 
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	const basePath = "templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
